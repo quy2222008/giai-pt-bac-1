2953d6fa50a2c24ce4869e6d04c164a1ed58d819
ADMIN <ADMIN@LAB1-MAY08>
2024-02-19 16:38:35 +0700

    Add project files.

---
+++ giai pt bac 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std; 
+int main() {
+	int a, b;
+	float x;
+	cout << "UNG DUNG GIAI PHUONG TRINH BAC I\n";
+	cout << "Nhap a:"; cin >> a;
+	cout << "Nhap b:"; cin >> b;
+	if (a == 0)
+	{
+		if (b == 0)
+	
+		{
+			cout << "PT dung voi moi x ~ vo so nghiem\n";
+		}
+		else {
+			cout << "PT TRINH VO NGHIEM\n";
+		}
+		
+	}
+	else {
+		x = (float)-b/a;
+		cout << "ket qua x= " << x << endl;
+	}
+	return 0;
+}
